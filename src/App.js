4d67870e15ccd4e8af912b63ff3b75613786dd56
Ryu <rmruthui@hotmail.com>
2019-09-02 19:29:08 -0700

    feat: added basic nav and logo component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Navigation from '../src/components/navigation/Navigation';
+import Logo from '../src/components/logo/Logo';
 
-function App() {
-  return (
-    <div className="App">
-      {/*<Navigation />
-      <Logo />
-      <ImageLinkForm />
-      <FaceRecognition /> */}
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <Logo />
+        { /*
+        <ImageLinkForm />
+        <FaceRecognition /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/logo/Logo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import faceIcon from './facial-recognition-icon.png';
+import './Logo.css';
+
+
+const Logo = () => {
+  return (
+    <div className='ma4 mt0'>
+      <Tilt className="Tilt br2 shadow-2" options={{ max : 25 }}>
+        <div className="Tilt-inner pa3">
+          <img alt='icon not found' src={ faceIcon }></img>
+        </div>
+      </Tilt>
+    </div>
+  );
+};
+
+export default Logo;
