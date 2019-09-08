f2cd42abc408da551ee21357cebb787ab7d38f27
Ryu <rmruthui@hotmail.com>
2019-09-07 18:56:54 -0700

    feat: Added Clarifi face recgnition, and functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
 import React, { Component } from 'react';
-import './App.css';
+import Clarifai from 'clarifai';
 import Particles from 'react-particles-js';
 import particleOptions from './particlesOptions';
-import Navigation from '../src/components/navigation/Navigation';
-import Logo from '../src/components/logo/Logo';
-import ImageLinkForm from '../src/components/imageLinkForm/ImageLinkForm';
 import Rank from '../src/components/rank/Rank';
+import Logo from '../src/components/logo/Logo';
+import Navigation from '../src/components/navigation/Navigation';
+import ImageLinkForm from '../src/components/imageLinkForm/ImageLinkForm';
+import FaceRecognition from '../src/components/faceRecognition/FaceRecognition';
+import './App.css';
+
+const app = new Clarifai.App({ apiKey: '2743498df09f424f9870c8879539ef9e' });
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imgUrl: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  }
+
+  onBtnSubmit = () => {
+    this.setState({ imgUrl: this.state.input });
+    if (this.state.input) {
+      app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input )
+        .then((response) => {
+          console.log('response: ', response.outputs[0].data.regions[0].region_info.bounding_box);
+        }).catch((err) => {
+          // there was an error
+        }
+      );
+    }
+  }
+
   render() {
     return (
       <div className='App'>
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm
+          onInputChange={ this.onInputChange } onBtnSubmit={ this.onBtnSubmit }
+        />
         <Particles className='particles' params={ particleOptions } />
-        { /* <FaceRecognition /> */ }
+        <FaceRecognition imgUrl={ this.state.imgUrl } />
       </div>
     );
   }
