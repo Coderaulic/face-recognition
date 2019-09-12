a86e88815396a20b58571239c657126e2ccb54f9
Ryu <rmruthui@hotmail.com>
2019-09-12 01:10:02 -0700

    refactor: minor code formatting

--- src/components/imageLinkForm/ImageLinkForm.js
+++ src/components/imageLinkForm/ImageLinkForm.js
@@ -4,12 +4,14 @@ import './ImageLinkForm.css';
 const ImageLinkForm = ({ onInputChange, onBtnSubmit }) => {
   return (
     <div>
-      <p className='white f3'>{ 'This app will detect faces in your pictures. Give it a try.' }</p>
+      <p className='white f3'>
+        { 'This app will detect faces in your images. Give it a try.' }
+      </p>
       <div className='center'>
         <div className='form pa4 center br3 shadow-5'>
           <input
             type='text'
-            placeholder={ 'Enter URL...'}
+            placeholder={ 'Enter URL...' }
             className='f4 pa2 w-70 center'
             onChange={ onInputChange }
           />
